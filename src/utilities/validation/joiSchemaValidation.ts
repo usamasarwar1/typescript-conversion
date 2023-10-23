8f61eb1f8f8da632e80501d1b1b97c241d199607
usama-sarwar <usama@litta.co>
2023-10-23 12:03:33 +0500

    fixes

--- src/utilities/validation/joiSchemaValidation.ts
+++ src/utilities/validation/joiSchemaValidation.ts
@@ -2,18 +2,17 @@ import { Request, Response, NextFunction } from "express";
 import { defaultServerResponse } from "../../utilities/common/response";
 import { requestValidationMessage } from "../../utilities/common/validation_message";
 import { errors } from "../../utilities/error";
-// import { validateObjectSchema } from "your-validation-library"; // Import the appropriate validation library
 
 const validateBody = (schema: any) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    // let response: any = defaultServerResponse;
-    // const error = validateObjectSchema(req.body, schema); // Replace 'any' with the appropriate type for your validation schema
-    // if (error) {
-    //   response.body = error;
-    //   response.message = requestValidationMessage.REQUEST_PAYLOAD_ERROR;
-    //   response.status = errors.Bad_Request.code;
-    //   return res.status(response.status).send(response);
-    // }
+    let response: any = defaultServerResponse;
+    const error = validateObjectSchema(req.body, schema); // Replace 'any' with the appropriate type for your validation schema
+    if (error) {
+      response.body = error;
+      response.message = requestValidationMessage.REQUEST_PAYLOAD_ERROR;
+      response.status = errors.Bad_Request.code;
+      return res.status(response.status).send(response);
+    }
     return next();
   };
 };
